32c12308907665e076d3fb413e5ec57115486539
wst <wstcode@gmail.com>
2012-09-05 17:21:05 +0200

    navigation through image pages

--- botserver/cherryserver.py
+++ botserver/cherryserver.py
@@ -10,23 +10,44 @@ class ImageListBuilder(object):
 
     IMAGES_DIR = os.path.join(SERVER_PATH, "images")
 
-    def build(self, template):
+    def build(self, template, page=None):
         filelist = os.listdir(self.IMAGES_DIR) 
+        images_html = ""
+
+        # no images
         if len(filelist) <= 0: 
             print("No image files found!")
-        
-        path_list = os.path.join(self.IMAGES_DIR, filelist[-1])
-        fp = open(path_list, "r")
-        image_list = fp.readlines()
+            images_html = "No images yet!"
+        else:
+            if page is not None and page in filelist:
+                shown_page = page
+            else:
+                shown_page = filelist[-1]
 
-        images_html = ""
-        for url in image_list:
-            if url[-1] == os.linesep:
-                url = url[:-1]
-            images_html += '<p><img src="{0}" alt="Some image" /></p>\n'.format(url)
-            images_html += "<hr />\n"
+            pos_in_list = filelist.index(shown_page)
 
-        fp.close()
+            path_list = os.path.join(self.IMAGES_DIR, shown_page)
+            fp = open(path_list, "r")
+            image_list = fp.readlines()
+
+            # insert links to previous and next page
+            images_html += "<p>\n"
+            if pos_in_list > 0:
+                images_html += '''<a href="/images/{0}" title="previous page"><- previous page
+                    </a>&nbsp;\n'''.format(filelist[pos_in_list - 1])
+            if pos_in_list < len(filelist) - 1:
+                images_html += '''<a href="/images/{0}" title="next page">next page ->
+                    </a>\n'''.format(filelist[pos_in_list + 1])
+            images_html += "</p>\n"
+
+            # insert images
+            for url in image_list:
+                if url[-1] == os.linesep:
+                    url = url[:-1]
+                images_html += '<p><img src="{0}" alt="Some image" /></p>\n'.format(url)
+                images_html += "<hr />\n"
+
+            fp.close()
 
         return template.replace("{{images}}", images_html)
 
@@ -35,9 +56,9 @@ class CherryServer(object):
     def index(self):
         return self.get_template("index.html")
         
-    def images(self):
+    def images(self, page=None):
         builder = ImageListBuilder() 
-        return builder.build(self.get_template("images.html"))
+        return builder.build(self.get_template("images.html"), page)
 
     def get_template(self, name):
         try:
