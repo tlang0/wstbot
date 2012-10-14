58a6688ac901890d83fc27d90eadff54fec4fcf9
wst <wstcode@gmail.com>
2012-10-14 13:24:13 +0200

    new irc client code, not tested

--- wstbot_irc.py
+++ wstbot_irc.py
@@ -25,28 +25,10 @@ import os
 import importlib
 import configparser
 import logging
-from colors import C
+from colors import IRCFormats
 from util import apply_seq
 from wstbot_locals import STREAM_LOG_FORMAT, FILE_LOG_FORMAT
-
-##### DIRECTORIES / FILE PATHS #####
-
-COMMANDS_DIR = "commands"
-PARSING_DIR = "parsing"
-FILE_LOG = "wstbot.log"
-
-###### ERROR MESSAGES ######
-
-DEFAULTERROR = "An error occurred!"
-
-##### MESSAGES ######
-
-QUITMSG = "bye."
-HELPMSG = 'Type !help to see a list of commands.'
-HELLOMSG = 'Hello, #CHANNEL! ' + HELPMSG
-WELCOMEMSG = 'Hello, #NICK!'
-FORTUNEMSG = "Your fortune for today is:\n#FORTUNE"
-NO_HELP_MSG = "There is no help message for this command!"
+from wstbot import WstBot
 
 logger = logging.getLogger(__name__)
 
@@ -63,73 +45,17 @@ def wstbot_load(debug=False):
     ident = parser.get(category, "ident")
     realname = parser.get(category, "realname")
     channel = parser.get(category, "channel")
-    wstbot_server_port = int(parser.get("server_config", "port"))
 
-    return WstBot(server, nick, port, ident, realname, channel, server_port=wstbot_server_port, debug=debug)
+    return WstBotIRC(server, nick, port, ident, realname, channel)
 
-class WstBot(wirc.wIRC):
+class WstBotIRC(wirc.wIRC):
 
-    def __init__(self, server, nick, port, ident, realname, channel, 
-            server_port=8111, debug=False):
+    def __init__(self, server, nick, port, ident, realname, channel):
         wirc.wIRC.__init__(self, server, nick, port, ident, realname, debug)
         self.silent = False
         self.chan = channel
-        self.server_port = server_port
-
-        # initialize logger
-        if debug:
-            logger.setLevel(logging.DEBUG)
-        else:
-            logger.setLevel(logging.INFO)
-        # stream handler
-        stream_handler = logging.StreamHandler()
-        stream_formatter = logging.Formatter(STREAM_LOG_FORMAT)
-        stream_handler.setFormatter(stream_formatter)
-        # file handler
-        file_handler = logging.FileHandler(FILE_LOG)
-        file_formatter = logging.Formatter(FILE_LOG_FORMAT)
-        file_handler.setFormatter(file_formatter)
-        # add handlers
-        logger.addHandler(stream_handler)
-        logger.addHandler(file_handler)
-
-        # load modules
-        self.commands = self.objects_from_files(COMMANDS_DIR)
-        self.keywords = self.objects_from_files(PARSING_DIR)
-
-    def objects_from_files(self, directory):
-        """
-        1. Read files from a folder
-        2. Create objects from the classes contained in the files which should
-           have the same name as the file, with the first letter in upper case
-
-        """
-        
-        objects = []
-
-        # Load object objects
-        for objectfile in os.listdir(directory):
-            if objectfile[-2:] != "py" or objectfile[0] in [".", "_"]:
-                continue
-
-            objectclass = objectfile[0].upper() + objectfile[1:objectfile.rfind('.')]
-            objectmodule = objectclass.lower()
-
-            # omit templates (abstract classes)
-            if objectmodule == "command" or objectmodule == "parser":
-                continue
-
-            try:
-                logger.info("Importing object '" + objectclass + "'...")
-                module = importlib.import_module("{0}.{1}".format(directory, objectmodule))
-                class_ = getattr(module, objectclass)
-                obj = class_(self, logger)
-                objects.append(obj)
-            except ImportError as err:
-                logger.warning("Importing '{0}' from '{1}' was unsuccessful!".format(objectclass, objectfile))
-                logger.warning("Reason: {}".format(err))
-
-        return objects
+        self.wstbot = WstBot(self, debug=True)
+        self.msg_formats = IRCFormats()
 
     # Send a formatted message
     def formatted_msg(self, chan, msg, addcolor=True):
@@ -148,82 +74,20 @@ class WstBot(wirc.wIRC):
     def chanmsg(self, msg, addcolor=True):
         self.formatted_msg(self.chan, msg, addcolor)
 
-    def get_command_object(self, cmd):
-        if cmd.strip() == "":
-            return None
-        for cmd_obj in self.commands:
-            if cmd == cmd_obj.get_cmd():
-                return cmd_obj
-        return None
-
+    def send_room_message(self, message):
+        self.chanmsg(message)
+    
     # Handle privmsg
     def on_privmsg(self, nick, ident, server, target, msg):    
-        # parsing. accept direct messages too
-        if msg is None:
-            logger.warning("on_privmsg: msg was None")
-        if msg == "":
-            logger.warning("on_privmsg: msg was empty")
-        if msg[0] != "!":
-            # check for keywords
-            for cmd_obj in self.keywords:
-                self.chanmsg(cmd_obj.do_parse(msg, nick))
-
-            return
-
-        # don't accept whispered and very short commands
-        if target != self.chan or len(msg) <= 2:
-            return
-
-        firstword = msg
-        argstr = "" # string with arguments
-        # if message contains more than one word
-        if " " in msg:
-            firstword = msg[:msg.find(" ")]
-            argstr = msg[msg.find(" ")+1:]
-        # user command
-        ucmd = firstword[1:]
-        # empty command?
-        if ucmd.strip() == "":
-            return
-        
-        # !help command
-        if firstword == "!help":
-            # try to get help for a specific command
-            if " " in msg:
-                cmd_obj = self.get_command_object(msg.split()[1])
-                if cmd_obj:
-                    help_msg = cmd_obj.get_help()
-                    if help_msg is not None and help_msg.strip() != "":
-                        self.chanmsg(help_msg)
-                    else:
-                        self.chanmsg(NO_HELP_MSG)
-                        
-            # general help
-            else:
-                cmds = ""
-                for cmd in self.commands:
-                    cmds += "!" + cmd.get_cmd() + ", "
-                self.chanmsg("Commands: " + cmds[:-2])
-                self.chanmsg("Also try !help [command] (without !)")
-        else:
-            # check for command
-            cmd_obj = self.get_command_object(ucmd)
-            if cmd_obj:
-                self.chanmsg(cmd_obj.do_cmd(argstr, nick))
+        self.wstbot.handle_message(nick, msg)
 
     # Joining a channel
     def on_me_join(self, channel):
-        hello_msg = HELLOMSG.replace('#CHANNEL', channel)
-        self.chanmsg(hello_msg)
+        self.wstbot.on_me_join(channel)
         
     # Someone else joins a channel
     def on_join(self, nick, ident, server):
-        welcome_msg = WELCOMEMSG.replace("#NICK", nick)
-        fortune_cmd_obj = self.get_command_object("fortune")
-        if fortune_cmd_obj:
-            fortune = fortune_cmd_obj.do_cmd("", nick)
-            welcome_msg += " " + FORTUNEMSG.replace("#FORTUNE", fortune)
-        self.chanmsg(welcome_msg)
+        self.wstbot.on_join(nick)
         
     # Handle all received data
     def on_receive(self, line):
@@ -231,7 +95,7 @@ class WstBot(wirc.wIRC):
         if "End of" in line and "376" in line:
             self.join(self.chan)
 
-    def provide_special_options(self):
+    def cli_options(self):
         print("Commands: msg, join")
         try:
             command = input()
@@ -246,35 +110,17 @@ class WstBot(wirc.wIRC):
                 self.join(argstr)
                 self.chan = argstr
         except KeyboardInterrupt:
-            return -
-
-    def provide_special_options(self):
-        print("Commands: msg, join")
-        try:
-            command = input()
-            sp = command.find(" ")
-            c = command[:sp]
-            argstr = command[sp + 1:]
-
-            if c == "msg":
-                self.send_room_message(argstr)
-            elif c == "join":
-                self.part(self.chan)
-                self.join(argstr)
-                self.chan = argstr
-        except KeyboardInterrupt:
-            return -11
+            return None
 
 if __name__ == '__main__':
-    wstbot = wstbot_load(debug=True)
-    wstbot.connect()
+    bot = wstbot_load(debug=True)
+    bot.connect()
 
     while True:
         try:
-            wstbot.doirc()
+            bot.doirc()
         except KeyboardInterrupt:
-            o = wstbot.provide_special_options()
-            if o == -1:
-                logger.info("bye!")
-                wstbot.quit()
+            o = bot.cli_options()
+            if o is None:
+                bot.quit()
                 break
