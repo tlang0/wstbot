d5fd433b1cc6c3ffbe00f576f4b4456646be30f1
wst <wstcode@gmail.com>
2012-10-15 22:15:34 +0200

    add module that looks for image links and shows the images (xmpp only)

---
+++ parsing/imagedisplay.py
@@ -0,0 +1,13 @@
+import re
+from wstbot_locals import URL_REGEX_PREFIX
+from parsing.parser import Parser
+
+OH_NO = "Your Jabber client doesn't show images. Change that."
+
+class Imagedisplay(Parser):
+    
+    def parse(self, msg, nick):
+        img_url_match = re.search(URL_REGEX_PREFIX + "\S+\.(png|jpg|jpeg|gif)", msg)
+        if img_url_match:
+            img_url = img_url_match.group(0)
+            return '<img src="{0}" alt="{1}" />'.format(img_url, OH_NO)
