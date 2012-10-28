06d199c9cc0cbd13457a565d21d4a4efb94512b9
wst <wstcode@gmail.com>
2012-10-28 18:02:03 +0100

    tried to fix scroll-loading

--- botserver/www/js/media_autoload.js
+++ botserver/www/js/media_autoload.js
@@ -1,6 +1,12 @@
 $(function() {
+    var just_reached_bottom = false;
+
     $(document).scroll(function() {
+        if (just_reached_bottom === true) {
+            return;
+        }
         if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            just_reached_bottom = true;
             load_content();
         }
     });
@@ -8,10 +14,15 @@ $(function() {
     function load_content() {
         var page = $("#media-content").attr("data-page");
         page = parseInt(page) - 1
+        // reached the end?
+        if (page <= 0) {
+            return;
+        }
         $.get("/media/page/" + page, function(data) {
             $("#media-content").append(data);
         });
         $("#media-content").attr("data-page", page);
+        just_reached_bottom = false;
     }
 
     $("#load-button").click(load_content);
