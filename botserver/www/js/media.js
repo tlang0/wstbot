56a3f542433abeeffde95db859215e790bee2a5e
wst <wstcode@gmail.com>
2012-11-02 02:16:39 +0100

    made js apply to conventions and fixed syntax errors; delete now also works with ajax-loaded items and the script doesn't endlessly send request when the ultimate bottom is reached

--- botserver/www/js/media.js
+++ botserver/www/js/media.js
@@ -1,51 +1,62 @@
-$(function() {
-    var ITEMS_PER_PAGE = 15;
-    var just_reached_bottom = false;
-    var delete_button = $(".delete-button");
+$(function () {
+    var ITEMS_PER_PAGE = 15,
+        LOADED_ALL = false,
+        justReachedBottom = false,
+        mediaContent = $("#media-content");
 
-    $(document).scroll(function() {
-        if (just_reached_bottom === true) {
-            return;
-        }
-        if ($(window).scrollTop() + $(window).height() >= $(document).height() * 0.90) {
-            just_reached_bottom = true;
-            load_content();
-        }
-    });
+    function deleteItemClick() {
+        var button = $(this),
+            id = button.attr("id");
 
-    function load_content() {
-        var nr = $("#media-content").attr("data-nr");
-        nr = parseInt(nr) + ITEMS_PER_PAGE
-        // reached the end?
-        $.get("/media/load/" + nr, function(data) {
-            $("#media-content").append(data);
-            $("#media-content").attr("data-nr", nr);
-            if (data == "") {
-                stop();
-            }
-            just_reached_bottom = false;
-        });
-    }
-
-    function stop() {
-        $("#load-button").hide();
-    }
-
-    $("#load-button").click(load_content);
-
-    delete_button.click(function() {
-        var button = $(this)
-        var id = button.attr("id");
-        var del = confirm("Delete item #" + id + "?");
-        if (del) {
-            $.post("/media/delete/" + id, function(data) {
+        if (confirm("Delete item #" + id + "?")) {
+            $.post("/media/delete/" + id, function (data) {
                 if (data === id) {
-                    var parent_li = button.parent("li");
-                    parent_li.next("hr").remove();
-                    parent_li.remove();
+                    var parentLi = button.parent("li");
+                    parentLi.next("hr").remove();
+                    parentLi.remove();
                 }
             });
         }
+    }
+
+    function bindDelete() {
+        $(".delete-button").unbind("click").click(deleteItemClick);
+    }
+    bindDelete();
+
+    function stop() {
+        $("#load-button").hide();
+        LOADED_ALL = true;
+        mediaContent.append("<p>You have reached the bottom! :)</p>");
+    }
+
+    function loadContent() {
+        var nr = mediaContent.attr("data-nr");
+        if (LOADED_ALL) {
+            return;
+        }
+        nr = parseInt(nr, 10) + ITEMS_PER_PAGE;
+        // reached the end?
+        $.get("/media/load/" + nr, function (data) {
+            mediaContent.append(data);
+            mediaContent.attr("data-nr", nr);
+            if (data === "") {
+                stop();
+            }
+            justReachedBottom = false;
+            bindDelete();
+        });
+    }
+
+    $(document).scroll(function () {
+        if (justReachedBottom || LOADED_ALL) {
+            return;
+        }
+        if ($(window).scrollTop() + $(window).height() >= $(document).height() * 0.90) {
+            justReachedBottom = true;
+            loadContent();
+        }
     });
 
+    $("#load-button").click(loadContent);
 });
