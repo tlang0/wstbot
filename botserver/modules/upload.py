d81450b8bc31e244baaf0a3ac2d67d44cb5ee22b
honboubao <ctsaplg@wolke7.net>
2014-07-28 01:09:27 +0200

    added upload module

---
+++ botserver/modules/upload.py
@@ -0,0 +1,111 @@
+########################################################################
+# Copyright 2012 honboubao, ctsaplg@wolke7.net
+#
+# This file is part of wstbot.
+#
+# wstbot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wstbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with wstbot. If not, see <http://www.gnu.org/licenses/>.
+########################################################################
+
+import os
+import cherrypy
+import uuid
+import mimetypes
+from urllib.parse import quote_plus
+from urllib.parse import unquote_plus
+from botserver.util import get_template_content
+from string import Template
+from cherrypy.lib.static import serve_file
+from cgi import escape
+
+DEFAULT_FILE = "upload.html"
+localDir = os.path.dirname(__file__)
+absDir = os.path.join(os.getcwd(), localDir)
+dir = "files"
+
+class Uploader:
+    """File upload page"""
+    
+    @cherrypy.expose
+    def index(self):
+        html_template = get_template_content(DEFAULT_FILE)
+        template = Template(html_template);        
+        return template.substitute(actionUrl=(self.get_request_url() + "submit"))
+        
+    @cherrypy.expose   
+    def submit(self, file):
+        url = self.get_request_url();
+        url = url[0:url.find("submit")]
+        
+        filename = quote_plus(uuid.uuid4().hex +  "_" + file.filename);           
+        savedFile = open(os.path.join(absDir, dir, filename), "wb")
+        while True:
+            data = file.file.read(8192)
+            if not data:
+                break            
+            savedFile.write(data)
+        savedFile.close()
+        
+        filename = quote_plus(filename)
+        out = "<div class='entry'>"
+        if (file.content_type.value.find("image") > -1): 
+            out +="<img src='" + url + "show/" + filename + "'>"            
+        out += "<label>"+ escape(file.filename) +"</label><br>\
+                File link: <a href='" + url + "show/" + filename + "'>" + url + "show/" + filename + "</a><br>\
+                Download link: <a href='" + url + "download/" + filename + "'>" + url + "download/" + filename + "</a><br>\
+                Delete link: <a href='" + url + "delete/" + filename + "'>" + url + "delete/" + filename + "</a>\
+                </div>"
+        return out;
+        
+    @cherrypy.expose   
+    def show(self, filename):
+        return self.serve_file(filename, False)
+        
+    @cherrypy.expose   
+    def download(self, filename):
+        return self.serve_file(filename, True)
+        
+    @cherrypy.expose   
+    def delete(self, filename):
+        path = os.path.join(absDir, dir, filename)
+        try:
+            os.remove(path)
+        except os.error as e:
+            return "Error: File not found. %s" % e
+        return "File succesfully deleted.";
+        
+    def get_request_url(self):
+        base = cherrypy.request.base
+        path = cherrypy.request.path_info;
+        if not path.endswith("/"):
+            path = path + "/"
+        return base + path
+    
+    
+    def serve_file(self, filename, download=False):
+        disposition = None
+        path = os.path.join(absDir, dir, filename)
+        mime =  mimetypes.guess_type(filename)[0]
+        if (not mime): 
+            mime = "application/octet-stream"
+        orgfilename = unquote_plus(filename[filename.find("_") + 1:]);
+        if (download):
+            disposition = "attachment"
+        try:
+            return serve_file(path, mime, disposition, orgfilename, True)
+        except cherrypy.NotFound as e:
+            return "Error: File not found."
+        
+def get():
+    builder = Uploader()
+    return builder
---
+++ botserver/www/js/upload.js
@@ -0,0 +1,151 @@
+function ready() {
+  document.getElementById('dropper').addEventListener('dragover', function (e) {              
+    e.preventDefault();
+  });
+  document.getElementById('dropper').addEventListener('drop', function (e) {   
+    e.preventDefault();
+    while (e.dataTransfer == null && e.originalEvent != null) {
+      e = e.originalEvent;
+    }
+    var files = e.dataTransfer.files;    
+    if (files) {           
+      send(files);
+    }
+    return false;
+  });
+      
+  registerOnPaste(document.getElementById('dropper'), function (data){
+    send([dataURItoBlob('data:text/plain,' + data)])
+  },  function (data){
+    send([dataURItoBlob(data)]);
+  })
+  .focus(); 
+}
+
+var dataURItoBlob = function (dataURI) {
+console.log(dataURI);
+  var data = dataURI.slice(dataURI.indexOf(',') + 1);
+  var desc = dataURI.split(',')[0].split(':')[1].split(';');
+  var mime = desc[0];  
+  var encoding = desc.length > 1 ? desc[desc.length - 1] : null;
+  if (encoding == 'base64') {
+    data = atob(data);
+  }
+  var array = [];
+  for(var i = 0; i < data.length; i++) {
+      array.push(data.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: mime});
+};
+
+var mime2ext = function(mime) {
+  switch(mime) {
+  case 'image/png':
+    return '.png';
+  case 'image/jpeg':
+    return '.jpg';
+  case 'image/gif':
+    return '.gif';
+  case 'text/plain':
+    return '.txt';
+  }
+  return '';
+}
+
+var send = function(files) {
+  var counter = 0;
+  for (var i=0; i < files.length; i++) {   
+    var xhr = new XMLHttpRequest();
+    xhr.file = files[i];
+    xhr.onreadystatechange = function(e) {
+      if ( 4 == this.readyState ) {
+        var el = document.getElementById('results');
+        el.innerHTML = this.responseText + el.innerHTML;
+        counter--;
+        if (counter == 0)
+          document.getElementById('counter').innerHTML = "";
+        else
+          document.getElementById('counter').innerHTML = "Uploading " + counter + " file" + (counter == 1 ? "" : "s") + ".";
+      }
+    };                        
+    xhr.open('post', url, true);
+   
+    var data = new FormData;
+    data.append('file', files[i], files[i].name || ('unknown' + mime2ext(files[i].type)));
+    counter++;
+    document.getElementById('counter').innerHTML = "Uploading " + counter + " file" + (counter == 1 ? "" : "s") + ".";
+    xhr.send(data);
+  }
+};
+
+// adapted from https://github.com/Puffant/paste.js
+var registerOnPaste = function(div, ontext, onimage) {
+  div.contentEditable = true;
+  var content = div.innerHTML;
+  var retrieving = false;
+  setInterval(function() {
+    if (!retrieving) {
+      div.innerHTML = content;
+    }
+  }, 1);
+  div.addEventListener('paste', function(ev) {
+    var clipboardData, item, text;
+    if (clipboardData = ev.clipboardData) {
+      // chrome
+      if (clipboardData.items) {
+        if (item = clipboardData.items[0]) {
+          if (item.type.match(/^image\//)) {
+            reader = new FileReader();
+            reader.onload = function(event) {
+              onimage(event.target.result);
+            };
+            reader.readAsDataURL(item.getAsFile());
+          }
+          item.getAsString(function(string) {
+              console.log(string);
+            });
+          if (item.type === 'text/plain') {
+            item.getAsString(function(string) {
+              ontext(string);
+            });
+          }
+        }
+      } 
+      // FF
+      else {
+        if (clipboardData.types.length) {
+          clipboardData.getData(function() {console.log(arguments)});
+          
+          if (text = clipboardData.getData('Text')) {
+            ontext(text);
+          }
+        } else {
+          retrieving = true;
+          setTimeout(function() {
+            var imgs = div.getElementsByTagName('img');
+            if (imgs.length > 0) {
+              onimage(imgs[0].src);
+              retrieving = false;
+            }
+          }, 1);
+        }
+      }
+    }
+    // IE
+    if (clipboardData = window.clipboardData) {
+      if (text = clipboardData.getData('Text')) {
+        ontext(text);
+      } else {        
+        retrieving = true;
+          setTimeout(function() {
+            var imgs = div.getElementsByTagName('img');
+            if (imgs.length > 0) {
+              onimage(imgs[0].src);
+              retrieving = false;
+            }
+          }, 1);
+      }
+    }
+  });
+  return div;
+};
